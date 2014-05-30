dce287ae4ae391e91d17f9ed80cf7ea5b0f72a76
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-30 10:17:19 -0400

    Create the review results function

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ Gifjs.prototype.blastOff = function(){
 
 }
 
-Gifjs.prototype.reviewAllNodes = function(){
-  var all = document.getElementsByTagName("*");
+Gifjs.prototype.getGifjsNodes = function(){
+  if (document.querySelectorAll){
+    // This is more performant
+    var gifElements = document.querySelectorAll("*[data-gifjs]");
+  } else {
+    // This works in all browsers
+    var gifElements = document.getElementsByTagName("*");
+  }
+  this.reviewAllResults(gifElements);
+}
+
+Gifjs.prototype.reviewAllResults = function(gifElements){
 
   for (var i=0, max=all.length; i < max; i++) {
 
   }
+
 }
