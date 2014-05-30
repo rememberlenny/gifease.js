f054b12778a3f3abaebcd1bdf0804e8406bbb3e7
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-30 10:12:17 -0400

    Review all nodes

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var Gifjs = {};
 Gifjs.prototype.blastOff = function(){
 
 }
+
+Gifjs.prototype.reviewAllNodes = function(){
+  var all = document.getElementsByTagName("*");
+
+  for (var i=0, max=all.length; i < max; i++) {
+
+  }
+}
